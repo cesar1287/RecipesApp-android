56010aa9fe338d3e41f7840c4785933ce348c1dd
César Rodrigues <rasec1287@hotmail.com>
2017-04-24 13:39:22 -0300

    renamed MainActivity to listViewActivity

--- app/src/main/java/comcesar1287/github/www/recipesapp/view/listViewActivity.java
+++ app/src/main/java/comcesar1287/github/www/recipesapp/view/listViewActivity.java
@@ -3,11 +3,11 @@ package comcesar1287.github.www.recipesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class listViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_view);
     }
 }
