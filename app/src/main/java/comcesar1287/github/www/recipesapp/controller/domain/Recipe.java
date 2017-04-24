ccd499ef54748c1f1976d47f35ed92e12bf6015c
César Rodrigues <rasec1287@hotmail.com>
2017-04-24 13:46:42 -0300

    added directory controller

---
+++ app/src/main/java/comcesar1287/github/www/recipesapp/controller/domain/Recipe.java
@@ -0,0 +1,68 @@
+package comcesar1287.github.www.recipesapp.controller.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Recipe {
+
+    private String id, name, image;
+    private int time;
+    private ArrayList<String> bookmarks, comments;
+    private HashMap<String,String> categories;
+
+    public ArrayList<String> getBookmarks() {
+        return bookmarks;
+    }
+
+    public void setBookmarks(ArrayList<String> bookmarks) {
+        this.bookmarks = bookmarks;
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<String> comments) {
+        this.comments = comments;
+    }
+
+    public HashMap<String, String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(HashMap<String, String> categories) {
+        this.categories = categories;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/comcesar1287/github/www/recipesapp/controller/util/Util.java
@@ -0,0 +1,6 @@
+package comcesar1287.github.www.recipesapp.controller.util;
+
+public class Util{
+
+    public static final String URL = "http://inshake.herokuapp.com/v1/recipes";
+}
